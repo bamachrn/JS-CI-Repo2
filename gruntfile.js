fbe55e648fa04fbd813b9e8da662f1aa13d896eb
user7 <vaisudpush@cyage.com>
2015-04-02 17:31:57 +0530

    new commit

---
+++ gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+      
+      jshint: {
+      files: ['./lib/hello.js','./lib/index.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+	
+      
+    uglify: {
+      options: {
+      mangle: false
+        },
+        
+    my_target: {
+      files: {
+        'dest/output.min.js': ['./lib/hello.js','./lib/index.js']
+      }
+    }
+  },
+
+      /*env: {
+      coverage: {
+        APP_DIR_FOR_CODE_COVERAGE: '../test/coverage/instrument/app/'
+      }
+    },*/
+      
+      instrument: {
+      files: 'lib/*.js',
+      options: {
+        lazy: true,
+        basePath: 'test/coverage/instrument/'
+      }
+    },
+      
+      simplemocha: {
+      options: {
+          globals: ['chai'],
+          timeout: 3000,
+          ignoreLeaks: false,
+          reporter: 'spec'
+    },
+
+    all: { src: ['test/*-test.js'] }
+  },
+      
+    storeCoverage: {
+      options: {
+        dir: 'test/coverage/reports'
+      }
+    },
+    makeReport: {
+      src: 'test/coverage/reports/**/*.json',
+      options: {
+        type: 'lcov',
+        dir: 'test/coverage/reports',
+        print: 'detail'
+      }
+    }
+      
+      
+    
+      /*plato: {
+  your_task: {
+    options : {
+      jshint : grunt.file.readJSON('./node_modules/grunt-plato/.jshintrc')
+     // simplemocha: grunt.file.readJSON('./node_modules/grunt-plato/test/plato_test.js')
+    },
+    files: {
+      'reports': ['./lib/hello.js','./lib/index.js']
+    }
+  }
+}*/
+                   
+
+      
+      
+});
+ grunt.registerTask('coverage', ['instrument', 'mochaTest', 'storeCoverage', 'makeReport']);
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+ // grunt.loadNpmTasks('grunt-plato');
+    grunt.loadNpmTasks('grunt-istanbul');
+//  grunt.loadNpmTasks('grunt-mocha');
+
+
+  grunt.registerTask('default', ['jshint','uglify','simplemocha',/*'plato' */,'coverage'])
+
+};
