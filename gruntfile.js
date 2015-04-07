5cfbe2e59f8862c8a497a656c1714701e844fe2e
Vaibhav Jain <vaibhavjai@cybage.com>
2015-04-07 18:48:16 +0530

    grunt changed mochaoption removed

--- gruntfile.js
+++ gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
           globals: ['chai'],
           timeout: 3000,
           ignoreLeaks: false,
-          reporter: 'mochawesome --no-exit'
+          reporter: 'mochawesome'
     },
 
     all: { src: ['test/*-test.js'] }
